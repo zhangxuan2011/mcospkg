d918cbdf16d9d237c9b08daedc178f32200788b9
zhangxuan2011 <zx20110412@outlook.com>
2024-10-30 22:07:31 +0800

    Make main be full and added color module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import urllib.request as fetch	# Fetch files 
 import os	# To exec commands
 import sys	# To get arguments
+from modules import color	# To manage the program easily(this imports by folder 'modules').
 
 
 # Get arguments
@@ -20,5 +21,10 @@ if len(args) == 1:
 	'\tmcospkg install base:\tTo install a package called base'
 	'\n\tmcospkg remove apt:\tTo remove a package called apt\n'
 	'To report bugs, welcome to send email to <zx20110412@outlook.com> or open an issue in <https://github.com/zhangxuan2011/mcospkg>. Have a nice day!\n')
-elif len(args) == 2:
-	print('error: missing options or package name.')
+elif len(args) == 2:	# It must lost options or pkg name.
+	print(f'{color.red}error{color.end}: missing options or package name.\nEnter command without arguments to see help message.')
+else:	# So this is available command
+    options = args[1]	# Get options
+    match options:
+    	case 'install':
+    		...
---
+++ modules/color.py
@@ -0,0 +1,15 @@
+"""
+=====Description=====
+File in 'modules/color.py'
+This file is ONLY for show colorful fonts
+=====Usage=====
+if var 'text' is the message you wanna change color,
+use print(<color>.start + text + <color>.end) to print this message.
+"""
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+pink = '\033[35m'
+aqua = '\033[36m'
+end = '\033[m'
