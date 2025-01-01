5c77c1eb2e28f0875a44a73f5d3ef0d4da0fdf33
zhangxuan2011 <zx20110412@outlook.com>
2025-01-01 23:23:37 +0800

    Reset file struct

---
+++ src/lib/file.rs
@@ -0,0 +1,10 @@
+use std::fs::File;
+
+/// This function check is file exists. If yes, return true, else return false.
+pub fn check_file_exist(filename: &str) -> bool {
+    match File::open(filename) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
