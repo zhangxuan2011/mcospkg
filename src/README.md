cec59b855569c8350547e7515843ffa7429d58eb
zhangxuan2011 <zx20110412@outlook.com>
2025-02-09 14:20:05 +0800

    Fixed some strange errors and created some README

---
+++ src/README.md
@@ -0,0 +1,36 @@
+## Producers
+
+### C producers
+XiaoKuai <rainyhowcool@outlook.com>
+
+
+## Description
+
+This directory contains the source code
+
+Why create this? Let me explain it:
+
+## Structures
+
+### Rust structure
+
+`main/` (directory): The library of `main.rs`;
+
+`main.rs`: The main file (This exports the binary file `mcospkg`);
+
+`lib.rs`: The library contains the thing for all the binary;
+
+`info.rs`: Exports the binary `mcospkg-info`;
+
+`mirror.rs`: Exports the binary `mcospkg-mirror`;
+
+### C structure
+
+
+`ExtractLinux.c`: The file that extracts the package in Linux;
+
+`pmio.c`: 
+
+`TextAttributes.c`: The text attributes;
+
+`main.c`: The main file;
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /***************************************************************************
-*   Copyright (C)                                                         *
- *   Email:                                                                *
+ *   Copyright (C)                                                         *
+ *                                                                         *
+ *   Email:  rainyhowcool@outlook.com                                      *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -70,7 +71,7 @@ void registerRemoveInfo(char* work_path, char* package_name, char* version) { //
     free(unhook_file); // free memory space
     unhook_file = NULL;
 
-    int version_info_length = strlen("/etc/mcospkg/database/remove_info/") + package_name + 1;
+    int version_info_length = strlen("/etc/mcospkg/database/remove_info/") + strlen(package_name) + 1;
     char* version_info = (char*) malloc(version_info_length);
     snprintf(version_info, version_info_length, "/etc/mcospkg/database/packages.toml");
     FILE* version_info_file = fopen(version_info, "a");
